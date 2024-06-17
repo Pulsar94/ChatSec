09887b6f39b6212ad89e05db1684f16bfcef95be
Tao Solan <tao.solan@gmail.com>
2024-06-17 15:31:14 +0200

    adding some base room signal

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 import ssl
 import threading as thread
 import json_handler as jh
-import time
+from client_function import func
 from certificate import get_or_generate_cert
 
 CERT_FILE_SERVER = "key/server-cert.pem"
@@ -14,7 +14,6 @@ CERT_EXPIRATION_DAYS = 1
 
 class Client:
     def __init__(self):
-        
         self.context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
         self.cert_file, self.key_file = get_or_generate_cert(CERT_FILE_CLIENT, KEY_FILE_CLIENT, CERT_EXPIRATION_DAYS)
         self.context.load_verify_locations(CERT_FILE_SERVER)
@@ -22,6 +21,7 @@ class Client:
         self.context.verify_mode = ssl.CERT_REQUIRED
         
         self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.func = func()
 
     def connect(self, host, port):
         self.ssl_clientsocket = self.context.wrap_socket(self.clientsocket, server_hostname=host)
@@ -33,8 +33,15 @@ class Client:
     
     def listen(self):
         while True:
-            data = self.ssl_clientsocket.recv(1024).decode()
-            print("Server says: ", data)
+            received = self.ssl_clientsocket.recv(1024).decode()
+            if received != "":
+                data = jh.json_decode(received)
+                print("Server says: ", data)
+
+                for tag, callback in self.func.tag.items():
+                    if jh.compare_tag_from_socket(data, tag, callback, self.ssl_clientsocket):
+                        print("Executed callback for tag", tag)
+                        break
         
     def send(self, message):
         self.ssl_clientsocket.send(message.encode())
@@ -50,15 +57,15 @@ def main():
     print("Client is ready to send to server")
 
     client.send(jh.json_encode("create_room", {"name": "room1", "password": "1234"}))
-    print("Room created")
     print(client.ssl_clientsocket.recv(1024).decode())
+    
+    client.send(jh.json_encode("connect_room", {"name": "room1"}))
 
     client.send(jh.json_encode("room_message", {"room": "room1", "message": "Hello, world!"}))
-    #print("Message sent")
     print(client.ssl_clientsocket.recv(1024).decode())
     print(client.ssl_clientsocket.recv(1024).decode())
     
-
-    
+    client.send(jh.json_encode("room_disconnect", {"room": "room1"}))
+    print(client.ssl_clientsocket.recv(1024).decode())
     
 main()
---
+++ client_function.py
@@ -0,0 +1,7 @@
+import json_handler as jh
+
+class func:
+    def __init__(self):
+        self.tag = {}
+    
+   
